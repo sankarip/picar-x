a7af5eac50cffe34d3447a3e939c5e81c334ac16
sankarip <74119626+sankarip@users.noreply.github.com>
2022-01-16 19:37:36 -0800

    Add files via upload

---
+++ lib/calibration.py
@@ -0,0 +1,18 @@
+
+import sys
+sys.path.append(r'/home/pi/picar-x/lib')
+from utils import reset_mcu
+reset_mcu()
+
+from picarx_improved import Picarx
+import time
+
+
+if __name__ == "__main__":
+    try:
+        px = Picarx()
+        px.forward(30)
+        time.sleep(0.5)
+
+    finally:
+        px.forward(0)
