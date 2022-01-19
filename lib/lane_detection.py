ae829108e572e411be8a3890897cde4264950a4a
sankarip <74119626+sankarip@users.noreply.github.com>
2022-01-19 15:37:26 -0800

    Add files via upload

--- lib/lane_detection.py
+++ lib/lane_detection.py
@@ -31,9 +31,9 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     cv2.fillPoly(mask, polygon, 255)
     cropped_edges = cv2.bitwise_and(edges, mask)
     #cv2.imshow("cropped edges", cropped_edges)
-    rho = 1  # distance precision in pixel, i.e. 1 pixel
+    rho = 2  # distance precision in pixel, i.e. 1 pixel
     angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
-    min_threshold = 10  # minimal of votes
+    min_threshold = 20  # minimal of votes
     line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold,
                                     np.array([]), minLineLength=8, maxLineGap=4)
     #print(len(line_segments))
