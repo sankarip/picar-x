b650eff2530ab38a3612c0c185724a0179bf3322
sankarip <74119626+sankarip@users.noreply.github.com>
2022-01-19 14:37:38 -0800

    Add files via upload

--- lib/lane_detection.py
+++ lib/lane_detection.py
@@ -13,8 +13,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     lower_blue = np.array([60, 40, 40])
     upper_blue = np.array([150, 255, 255])
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    cv2.imshow("video", img)  # OpenCV image show
-    cv2.imshow("mask", mask)  # OpenCV image show
+    #cv2.imshow("video", img)  # OpenCV image show
+    #cv2.imshow("mask", mask)  # OpenCV image show
+    edges = cv2.Canny(mask, 200, 400)
+    cv2.imshow("edges", edges)
     rawCapture.truncate(0)  # Release cache
 
     k = cv2.waitKey(1) & 0xFF
