c9546b45ac7c52f89f4c9348a399d365d9e37691
sankarip <74119626+sankarip@users.noreply.github.com>
2022-01-18 20:11:22 -0800

    Add files via upload

--- lib/light_sensor.py
+++ lib/light_sensor.py
@@ -17,6 +17,7 @@ import sys
 sys.path.append(r'/home/pi/picar-x/lib')
 from utils import reset_mcu
 reset_mcu()
+from picarx_improved import Picarx
 
 class light_sensor(object):
     def __init__(self,ref = 1000):
@@ -52,17 +53,45 @@ class light_interpreter(object):
                 self.turn=-1
             elif val3-self.sensitivity*self.polarity>val2 and val1-self.sensitivity*self.polarity>val2:
                 print("lines on both sides detected")
+            elif val2-25<val3<val2+25 and val2-25<val1<val2+25:
+                print("no lines found")
+                self.turn=2
             else:
                 self.turn=0
             print(self.turn)
             return self.turn
 
+class light_controller(object):
+    def __init__(self):
+        car = light_interpreter(50, 1)
+        self.control=car.interp()
+    def light_steer(self):
+        if self.control==1:
+            px = Picarx()
+            px.set_dir_servo_angle(20)
+            time.sleep(0.5)
+            px.forward(30)
+            time.sleep(1)
+        elif self.control==-1:
+            px = Picarx()
+            px.set_dir_servo_angle(-20)
+            time.sleep(0.5)
+            px.forward(30)
+            time.sleep(1)
+        elif self.control==2:
+            px = Picarx()
+            px.forward(0)
+        else:
+            px = Picarx()
+            px.forward(30)
+            time.sleep(.5)
+
+
 if __name__ == "__main__":
     import time
     while True:
-        a=light_interpreter(50,1)
-        a.interp()
-        time.sleep(.5)
+        a=light_controller()
+        a.light_steer()
 
 
 
