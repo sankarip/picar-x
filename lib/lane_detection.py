614f528d2d7936d8b820ffae552c467b4a4183ec
sankarip <74119626+sankarip@users.noreply.github.com>
2022-01-21 14:36:00 -0800

    Add files via upload

--- lib/lane_detection.py
+++ lib/lane_detection.py
@@ -19,9 +19,14 @@ rawCapture = PiRGBArray(camera, size=camera.resolution)
 steering_angles=[]
 #look down
 px = Picarx()
-px.set_camera_servo2_angle(30)
+px.set_camera_servo2_angle(-25)
 time.sleep(1)
-
+px.set_camera_servo1_angle(30)
+time.sleep(1)
+px.forward(10)
+time.sleep(1)
+px.forward(0)
+time.sleep(3)
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):  # use_video_port=True
     img = frame.array
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
