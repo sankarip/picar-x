c857b6ada782b2e4cb4a2f0468803b864d358eaa
sankarip <74119626+sankarip@users.noreply.github.com>
2022-01-22 14:28:19 -0800

    Add files via upload

--- lib/lane_detection.py
+++ lib/lane_detection.py
@@ -21,19 +21,21 @@ steering_angles=[]
 px = Picarx()
 px.set_camera_servo2_angle(15)
 time.sleep(.2)
+#much of the following code is from the lane following tutorial in 3.5
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):  # use_video_port=True
+    #get image
     img = frame.array
+    #convert to hsv
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    #get blue
     lower_blue = np.array([60, 40, 40])
     upper_blue = np.array([150, 255, 255])
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    #cv2.imshow("video", img)  # OpenCV image show
     #cv2.imshow("mask", mask)  # OpenCV image show
     edges = cv2.Canny(mask, 200, 400)
     #cv2.imshow("edges", edges)
     height, width = edges.shape
     mask = np.zeros_like(edges)
-
     # only focus bottom half of the screen
     polygon = np.array([[
         (0, height * 1 / 2),
@@ -41,7 +43,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         (width, height),
         (0, height),
     ]], np.int32)
-
     cv2.fillPoly(mask, polygon, 255)
     cropped_edges = cv2.bitwise_and(edges, mask)
     #cv2.imshow("cropped edges", cropped_edges)
@@ -50,14 +51,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     min_threshold = 15  # minimal of votes
     line_segments = cv2.HoughLinesP(cropped_edges, rho, angle, min_threshold,
                                     np.array([]), minLineLength=5, maxLineGap=4)
-    #print(len(line_segments))
+    #commented out code for displaying the lines found by the hough transform
     #if line_segments is not None:
         #print(len(line_segments))
      #   for line in line_segments:
       #      x1, y1, x2, y2 = line[0]
        #     cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
     #cv2.imshow("lane lines", img)
-
+    #get the points of a line from its slope and intercept
     def make_points(h,w, line):
         height=h
         width=w
@@ -65,21 +66,19 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         if slope !=0:
             y1 = height  # bottom of the frame
             y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
-
             # bound the coordinates within the frame
             x1 = max(-width, min(2 * width, int((y1 - intercept) / slope)))
             x2 = max(-width, min(2 * width, int((y2 - intercept) / slope)))
         else:
             y1 = height  # bottom of the frame
             y2 = int(y1 * 1 / 2)  # make points from middle of the frame down
-
             # bound the coordinates within the frame
             x1 = max(-width, min(2 * width, int((y1 - intercept) / .5)))
             x2 = max(-width, min(2 * width, int((y2 - intercept) / .5)))
-
         return [[x1, y1, x2, y2]]
-
+    #get the height and width of the img
     height, width = edges.shape
+    #empty lists for grouping the lines
     left_fit = []
     right_fit = []
     lane_lines = []
@@ -89,7 +88,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     if line_segments is not None:
         for line_segment in line_segments:
             for x1, y1, x2, y2 in line_segment:
-
                 #fit = np.polyfit((x1, x2), (y1, y2), 1)
                 #slope = fit[0]
                 #intercept = fit[1]
@@ -108,23 +106,19 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
                 else:
                     if x1 > right_region_boundary and x2 > right_region_boundary:
                         right_fit.append((slope, intercept))
-
         left_fit_average = np.average(left_fit, axis=0)
         if len(left_fit) > 0:
             lane_lines.append(make_points(height,width, left_fit_average))
-
         right_fit_average = np.average(right_fit, axis=0)
         if len(right_fit) > 0:
             lane_lines.append(make_points(height,width, right_fit_average))
-    #print(lane_lines)
     if lane_lines is not None:
-        #print(len(line_segments))
         for line in lane_lines:
             x1, y1, x2, y2 = line[0]
             cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 3)
     #cv2.imshow("lane lines", img)
+    #if there are two lines
     if len(lane_lines) ==2:
-        #print(lane_lines)
         _, _, left_x2, _ = lane_lines[0][0]
         _, _, right_x2, _ = lane_lines[1][0]
         mid = int(width / 2)
@@ -135,12 +129,11 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         steering_angle = angle_to_mid_deg + 90 # this is the steering angle needed by picar front wheel
         #print(steering_angle)
         steering_angles.append(steering_angle)
+        #checking the steering angle against the previous angle to make it so that the car can't change its heading too much in a single reading
         if len(steering_angles)==2:
             ang1=steering_angles[0]
             ang2=steering_angles[1]
             #if its within 5 degrees
-            print("ang1: ", ang1)
-            print("ang2: ", ang2)
             if ang1-5<ang2<ang1+5:
                 steering_angle=steering_angle
             elif ang1-5>ang2:
@@ -153,13 +146,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
             steering_angles.append(steering_angle)
             # get rid of the old entry
             steering_angles.pop(0)
+        #code for visualizing the heading
         steering_angle_radian = steering_angle / 180.0 * np.pi
         x1 = int(width / 2)
         y1 = height
         x2 = int(x1 - height / 2 / np.tan(steering_angle_radian))
         y2 = int(height / 2)
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
-
         # convert to heading for car
         steering_angle = steering_angle - 90
         # drive a little bit
@@ -169,8 +162,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         time.sleep(.1)
         px.forward(0)
         time.sleep(.4)
-
-
     elif len(lane_lines) ==1:
         x1, _, x2, _ = lane_lines[0][0]
         x_offset = x2 - x1
@@ -178,18 +169,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         angle_to_mid_radian = np.arctan(x_offset / y_offset)  # angle (in radian) to center vertical line
         angle_to_mid_deg = int(angle_to_mid_radian * 180.0 / np.pi)  # angle (in degrees) to center vertical line
         steering_angle = angle_to_mid_deg + 90  # this is the steering angle needed by picar front wheel
-        steering_angle_radian = steering_angle / 180.0 * np.pi
-        x1 = int(width / 2)
-        y1 = height
-        x2 = int(x1 - height / 2 / np.tan(steering_angle_radian))
-        y2 = int(height / 2)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 5)
         steering_angles.append(steering_angle)
         if len(steering_angles)==2:
             ang1=steering_angles[0]
             ang2=steering_angles[1]
-            print("ang1: ", ang1)
-            print("ang2: ", ang2)
             #if its within 5 degrees
             if ang1-5<ang2<ang1+5:
                 steering_angle=steering_angle
@@ -213,7 +196,6 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         steering_angle=steering_angle-90
         #drive a little bit
         px.set_dir_servo_angle(steering_angle)
-        print(steering_angle)
         time.sleep(0.5)
         px.forward(5)
         time.sleep(.1)
@@ -226,6 +208,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
         px = Picarx()
         px.forward(0)
         time.sleep(1)
+    #shwo an image that has the heading and the lane lines
     cv2.imshow("steering", img)
     rawCapture.truncate(0)  # Release cache
 
