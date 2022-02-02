d643a78e9dfaf6fa76f1131ee6036e2fe4971a43
sankarip <74119626+sankarip@users.noreply.github.com>
2022-02-02 13:59:56 -0800

    Add files via upload

--- lib/week5ROB599.py
+++ lib/week5ROB599.py
@@ -72,13 +72,13 @@ def interp_con_prod(sensordata):
 def consumer_controller(turnval):
     if turnval==1:
         px = Picarx()
-        px.set_dir_servo_angle(-22)
-        time.sleep(0.5)
+        px.set_dir_servo_angle(-20)
+        time.sleep(0.05)
     #turn right
     elif turnval==-1:
         px = Picarx()
-        px.set_dir_servo_angle(22)
-        time.sleep(0.5)
+        px.set_dir_servo_angle(20)
+        time.sleep(0.05)
     #dont move
     elif turnval==2:
         px = Picarx()
